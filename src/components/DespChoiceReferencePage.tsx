7e40d017d33b5f4de224fd65c388a9a9742c5c44
integraleacademy <clement.annecy@gmail.com>
2026-09-05 19:17:41 +0200

    Mettre à jour src/components/DespChoiceReferencePage.tsx

--- src/components/DespChoiceReferencePage.tsx
+++ src/components/DespChoiceReferencePage.tsx
@@ -255,6 +255,7 @@ export function DespChoiceReferencePage({ sessions }: { sessions: TrainingDatesP
       <TrainingDatesPricingSection
         sessions={sessions}
         theme="orange"
+        seatCapacity={20}
         showDeliveryPeriods
         showLocationFilter
         defaultPrice="À partir de 3 800 €"
